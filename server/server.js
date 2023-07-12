c73f41e01930a4aa062e59ee13cbf78badaef59b
Chazz Carrizales <czcarrizales@yahoo.com>
2023-07-11 20:06:16 -0700

    using correct PORT

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const axios = require('axios')
 const app = express()
+const PORT = proces.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.json())
@@ -23,6 +24,6 @@ app.post('/search', (req, res) => {
     })
 })
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     console.log('Server is running on port 5000')
 })
