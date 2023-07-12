1bd8a6b04a6e1074349309bd9b8db53212dd8e76
Chazz Carrizales <czcarrizales@yahoo.com>
2023-07-11 20:09:17 -0700

    fix typo on process.env.PORT

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const axios = require('axios')
 const app = express()
-const PORT = proces.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.json())
